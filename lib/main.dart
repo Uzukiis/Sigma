c2479841ef2df9de919c2b803bed246b6e93371f
Uzukiis <121286291+Uzukiis@users.noreply.github.com>
2024-10-30 15:58:52 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
+import 'main_navigator.dart';
 
 void main() {
   runApp(const LoginScreen());
@@ -11,21 +11,33 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromRGBO(33, 33, 33, 1)
+     theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(100, 240, 186, 123),
+        ),
       ),
       home: Scaffold(
-        body: Center(
-          child: Container(decoration: const BoxDecoration(color: Color.fromRGBO(253, 247, 239, 1) ,shape: BoxShape.circle,), // Color.fromRGBO(253, 247, 239, 1)
-            width: 250,
-            height: 250,
-            child: const Padding(padding:  EdgeInsets.all(50), child:Image(image: AssetImage('assets/img/sigma.jpg'),
-            width: 100,
-            height: 100,) 
-)
-          )
-        ),
+        body: LoginScreent()
       ),
     );
   }
 }
+  class LoginScreent extends StatelessWidget {
+  const LoginScreent ({super.key});
+    Widget build(BuildContext context) {
+    return Center(
+          child: Column(
+            children:[
+              const TextField(decoration: InputDecoration(
+              label: Text('Name')
+            )),
+                  
+              TextButton(
+              onPressed: () => Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => const MainApp())),
+              child: const Text('Enter'),
+            ),
+          ],),
+        );}
+
+  }
